586aa81b2ea6fd01e7cf573a97c0a6024f0cd24d
Kiersten Foulk <yoemail@iu.edu>
2025-02-10 22:58:55 -0500

    Further commits

--- index.js
+++ index.js
@@ -6,25 +6,24 @@
 // 3. either type journal entry or receive error response
 // 4. Commit changes to Github
 
-
+//import node.js modules for cli tool
 const readline = require("readline"); // Readline module for user input
 const fs = require("fs");  // File System module to create files and directories
 const path = require("path"); // Path module to manage and resolve file paths
 
-//
-
+// Using the readline module from node.js, the rl object below is for user input in the console
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-// Get file path
+// Get file path using the path module in node.js
 
 const logFilePath = path.join(__dirname, "journal.txt");
 
-// Response in console... type journal-log for journal entry after installing npm
+// Response in console
 
-// function for saving journal entry to file
+// function for saving journal entry to file using fs module built in node.js
 
 function logEntry(entry) {
     fs.appendFile(logFilePath, `[${new Date().toISOString()}] ${entry}\n`, (err) => {
@@ -33,5 +32,5 @@ function logEntry(entry) {
         rl.close();
     });
 }
-
+// Displays prompt for user input in the console using the UI created on lines 15-18
 rl.question("Write your journal entry: ", logEntry);
